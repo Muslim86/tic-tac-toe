e817e1dd4b9bdd63e29b2d91c061305d269fd27b
Muslim Temirkhanov <muslim.temirhanov_86@mail.ru>
2022-01-19 07:34:28 +0500

    Инициализация, первые компоненты

---
+++ src/components/playingField/playingArea.jsx
@@ -0,0 +1,19 @@
+import style from './playingArea.module.css';
+
+const PlayingArea = () => {
+    return (
+        <div className={style.area}>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default PlayingArea;
